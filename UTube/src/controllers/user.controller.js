049f61d9821413809e520936db3a552b174c3574
Shvam Singh <singhshivam583@yahoo.in>
2023-11-25 21:15:04 +0530

    update

---
+++ UTube/src/controllers/user.controller.js
@@ -0,0 +1,91 @@
+import {asyncHandler} from '../utils/asyncHandler.js';
+import { apiError } from '../utils/apiError.js';
+import { User } from '../models/user.model.js';
+import { uploadOnCloudinary } from '../utils/cloudinary.js';
+import { apiResponse } from '../utils/apiResponse.js';
+
+
+const registerUser = asyncHandler(async(req, res) => {
+    // res.status(200).json({
+    //     message: "OK",
+    // })
+    //  get user details from frontend
+    //  validation not empty
+    //  check user existance 
+    //  check for images and avtar
+    //  upload them to cloudinary , avatar check 
+    //  create user object - create entry in db 
+    //  remove password and refresh token field from response
+    //  return response 
+
+    const { fullName, email, username, password } = req.body
+    // console.log("email: ", email);
+
+    // if (fullName === "") {
+    //     throw new apiError(400, "Full Name is required")
+    // }
+
+    if (
+        [fullName, email, username, password].some((field) => field?.trim() ==="")
+    ){
+        throw new apiError(400, "All fields are required")
+    }
+
+    const existingUser = await User.findOne({
+        $or: [{email}, {username}]
+    })
+
+    if (existingUser) {
+        throw new apiError(409, "User already exist")
+    }
+    // req.body ...... multer middle gives req.file 
+    console.log(req.files);
+    const avatarLocalPath =req.files?.avatar[0]?.path;
+    // console.log(avatarLocalPath);
+
+    let coverImageLocalPath
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
+    // console.log(coverImageLocalPath);
+
+    if (!avatarLocalPath) {
+        throw new apiError(400, "Avatar file path is required")
+    }
+    // const avatarPath = '../../' + avatarLocalPath 
+    // const coverPath = '../../' + coverImageLocalPath 
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+    // console.log(avatar);
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+    // console.log(coverImage);
+
+    if (!avatar) {
+        throw new apiError(400, "Avatar file is required")
+    }
+
+    const user = await User.create({
+        fullName,
+        avatar: avatar.url,
+        coverImage: coverImage?.url || "",
+        email,
+        password,
+        username: username.toLowerCase()
+    })
+
+   const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+   )
+
+   if(!createdUser){
+        throw new apiError(500, "Something went wrong while registering user")
+   }
+
+   return res.status(201).json(
+    new apiResponse(200, createdUser, "User Registered Successfully")
+   )
+    
+})
+
+export {registerUser} 
---
+++ UTube/src/utils/cloudinary.js
@@ -0,0 +1,40 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from 'fs';
+import dotenv from 'dotenv';
+
+dotenv.config({
+    path: '.env',
+})
+
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET
+  });
+
+
+const uploadOnCloudinary = async (localFilePath) => {
+    // console.log(localFilePath);
+    try {
+        if (!localFilePath) return null;
+        // upload the  file on cloudinary
+        const response = await cloudinary.uploader.upload(
+            localFilePath, 
+            {resource_type: 'auto'}
+        )
+        //  file has been uploaded successfully 
+        // console.log("file uploaded successfully on cloudinary", response.url);
+        fs.unlinkSync(localFilePath)
+        return response;
+            
+    } catch (error) {
+        fs.unlinkSync(localFilePath) // remove the localy saved temp file as the upload operation failed
+        return null ;
+    }
+}
+
+export {uploadOnCloudinary}
+
+//   cloudinary.v2.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
+//   { public_id: "olympic_flag" }, 
+//   function(error, result) {console.log(result); });
