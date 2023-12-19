b13982fa859a6089dc4fe1f43fca87d8196a7c83
Shvam Singh <singhshivam583@yahoo.in>
2023-12-19 22:57:48 +0530

    update

--- UTube/src/middlewares/auth.middleware.js
+++ UTube/src/middlewares/auth.middleware.js
@@ -3,16 +3,17 @@ import { apiError } from "../utils/apiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { User } from "../models/user.model.js";
 
-
 export const verifyJWT = asyncHandler(async(req, _, next) => {
     try {
         const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
-    
+        // console.log(token);
+
         if(!token){
             throw new apiError(401, "unauthorized request")
         }
     
-       const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET) 
+       const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+       console.log(decodedToken);
     
        const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
     
