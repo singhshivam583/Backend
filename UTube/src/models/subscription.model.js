19a0e7530e7c12dd50727535f0b1fc9d5b342002
Shvam Singh <singhshivam583@yahoo.in>
2024-02-06 00:22:59 +0530

    update

--- UTube/src/models/subscription.model.js
+++ UTube/src/models/subscription.model.js
@@ -10,6 +10,6 @@ const subscriptionSchema = new Schema({
         ref: "User",
     }
     
-}, {timestamps: true})
+}, {timestamps: true}) 
 
 export const Subscription = mongoose.model("Subscription", subscriptionSchema)
